5125ce56da16ba83a6e32ac90f49d2d39d056c52
Ausaaf <arshad.ausaaf@gmail.com>
2021-09-28 08:55:01 -0500

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import books from './_files/books.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bookmaster';
+  public bookList:{id:string, title:string, isbn:string, pageCount:string, thumbnailUrl:string, shortDescription:string, longDescription:string,
+  status:string}[] = books;
 }
---
+++ src/app/books/books.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../book';
+import { BOOKS } from '../mock-books';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent implements OnInit {
+
+ books = BOOKS;
+  selectedBook?: Book;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSelect(book: Book): void {
+    this.selectedBook= book;
+
+}
+}
